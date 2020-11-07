0a56c9678e04d2d00211e4f8e90ecbb5376c0926
Tanner Larson <tanner.b.larson@gmail.com>
2020-11-06 18:04:05 -0700

    Before Base_Tests.py implementation:

--- Base_Tests.py
+++ Base_Tests.py
@@ -4,7 +4,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import *
-import Base_Tests as gt
 
 
 def wait_until(driver, func):
@@ -20,66 +19,71 @@ def wait_until(driver, func):
     return True
 
 
-class IndexPageTests(unittest.TestCase):
+class GlobalTests(unittest.TestCase):
     driver = None
+
+    def __init__(self, page_to_test, page_class):
+        super().__init__()
+        self.page_to_test = page_to_test
+        self.page_class = page_class
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
 
     def setUp(self) -> None:
-        self.driver.get("http://automationpractice.com/index.php")
-        self.homePage = page.HomePage(self.driver)
+        self.driver.get(self.page_to_test)
+        self.mainPage = self.page_class(self.driver)
 
-    def _test_is_index(self):
-        assert self.homePage.is_page_correct()
+    def _test_is_page_correct(self):
+        assert self.mainPage.is_page_correct()
 
-    def _test_title(self):
+    def test_title(self):
         print("Testing webpage title...")
-        assert self.homePage.is_title_matches()
+        assert self.mainPage.is_title_matches()
 
     def _test_search_button(self):
         print("Testing search button...")
-        self.homePage.click_search_button()
+        self.mainPage.click_search_button()
         search_result_page = page.SearchResultPage(self.driver)
         assert wait_until(self.driver, search_result_page.is_page_correct())
 
     def _test_top_ad(self):
         print("Testing advertisement at top of website")
-        self.homePage.click_ad_top()
+        self.mainPage.click_ad_top()
         time.sleep(0.5)  # Give driver time to navigate
-        assert self.homePage.is_page_correct()
+        assert self.mainPage.is_page_correct()
 
     def _test_contact_us_button(self):
         print("Testing Contact us button")
-        self.homePage.click_contact_us_button()
+        self.mainPage.click_contact_us_button()
         contactPage = page.ContactPage(self.driver)
         assert contactPage.is_page_correct()
 
     def _test_sign_in_button(self):
         print("Testing Sign in button")
-        self.homePage.click_sign_in_button()
+        self.mainPage.click_sign_in_button()
         signInPage = page.SignInPage(self.driver)
         assert signInPage.is_page_correct()
 
     def _test_account_button(self):
         print("Testing Account button")
-        self.homePage.sign_in()
+        self.mainPage.sign_in()
         self.driver.get("http://automationpractice.com/index.php")
-        assert self.homePage.is_page_correct()
+        assert self.mainPage.is_page_correct()
 
-        self.homePage.click_account_button()
+        self.mainPage.click_account_button()
         accountPage = page.AccountPage(self.driver)
         assert accountPage.is_page_correct()
 
     def _test_sign_out_button(self):
         print("Testing Sign out button")
-        self.homePage.sign_in()
+        self.mainPage.sign_in()
         self.driver.get("http://automationpractice.com/index.php")
-        assert self.homePage.is_page_correct()
+        assert self.mainPage.is_page_correct()
 
-        self.homePage.click_sign_out_button()
-        assert self.homePage.is_page_correct()
+        self.mainPage.click_sign_out_button()
+        assert self.mainPage.is_page_correct()
 
     def _test_cart_button_empty(self):
         pass
@@ -95,8 +99,8 @@ class IndexPageTests(unittest.TestCase):
 
     def _test_logo_button(self):
         print("Testing logo button")
-        self.homePage.click_logo_button()
-        assert self.homePage.is_page_correct()
+        self.mainPage.click_logo_button()
+        assert self.mainPage.is_page_correct()
 
     def _test_women_button(self):
         pass
@@ -118,21 +122,5 @@ class IndexPageTests(unittest.TestCase):
         cls.driver.close()
 
 
-def run_global_and_home_unit_tests():
-    test_classes_to_run = [gt.GlobalTests("http://automationpractice.com/index.php",
-                                          page.HomePage), IndexPageTests]
-    loader = unittest.TestLoader()
-
-    suites_list = []
-    for test_class in test_classes_to_run:
-        suite = loader.loadTestsFromTestCase(test_class)
-        suites_list.append(suite)
-
-    big_suite = unittest.TestSuite(suites_list)
-
-    runner = unittest.TextTestRunner()
-    results = runner.run(big_suite)
-
-
-if __name__ == "__main__":
-    run_global_and_home_unit_tests()
+def execute():
+    unittest.main()
